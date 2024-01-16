e6874a35dd7605f23c340a1469a4093536d44cd2
MANISH007-GIF <143315274+MANISH007-GIF@users.noreply.github.com>
2024-01-16 18:29:25 +0530

    Update script.js

--- portfoliyo web/script.js
+++ portfoliyo web/script.js
@@ -1,3 +1,23 @@
+// auto typing text 
+const text = document.querySelector(".sec-text");
+
+const textLoad = () => {
+    setTimeout(() => {
+        text.textContent = "Developer";
+    }, 0)
+    setTimeout(() => {
+        text.textContent = "Designer";
+    }, 4000);
+    setTimeout(() => {
+        text.textContent = "Youtuber";
+    }, 8000);
+
+}
+
+textLoad();
+setInterval(textLoad, 12000);
+
+
 /*===== MENU SHOW =====*/ 
 const showMenu = (toggleId, navId) =>{
     const toggle = document.getElementById(toggleId),
